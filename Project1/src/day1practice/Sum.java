d4ea50e3a5b1b78cb3a784ad3f5d4dde71ac0693
Sanjok khadka <khadkasanjok02@gmail.com>
2023-05-25 17:33:18 +0545

    May 25 2023 sum file is added 5:30 PM

---
+++ Project1/src/day1practice/Sum.java
@@ -0,0 +1,15 @@
+package day1practice;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int num1, num2,sum;
+		 num1=10;
+		 num2=20;
+		sum=num1+num2;
+		System.out.println(num1+num2);
+
+	}
+	
+
+}
